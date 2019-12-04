08073cfe61f5dfd2b7defdcbab85adff2a3528da
Rowdy S <rowdysanford@gmail.com>
2019-12-03 17:22:38 -0800

    update views.py commented out @reqire_staff_status

--- simplesignage/manager/views.py
+++ simplesignage/manager/views.py
@@ -49,14 +49,14 @@ Video_Compiling = False
 #_______________________________________________________________________________
 # primary interface stuffs:
 #@login_required
-@staff_member_required
+###@staff_member_required
 def index(request):
     resp = HttpResponse("<h1>Error 909: Development Whilst Drunk Occured</h1><h2>apologies for any inconveniece</h2><hr>")
     return resp, redirect('/')
 
 # home / main data interface
 @login_required
-@staff_member_required
+###@staff_member_required
 def home(request):
     #print("Home", str(request.META['REMOTE_ADDR']))
     # content is passed into render, and can be parsed by the template
@@ -73,7 +73,7 @@ def documentation(request):
 #_______________________________________________________________________________
 # File dealings:
 @login_required
-@staff_member_required
+###@staff_member_required
 
 def upload_file(request):
     if request.method == 'POST':
@@ -242,7 +242,7 @@ def token_authorization(func):
 
 
 @login_required
-@staff_member_required
+##@staff_member_required
 def screens(request):
     print("SCREENS!")
 
@@ -277,7 +277,7 @@ def screen_active_hours(request):
     print("active hours")
 
 @login_required
-@staff_member_required
+##@staff_member_required
 def newScreen(request):
     # save a new screen
     screen = request.POST
@@ -294,7 +294,7 @@ def newScreen(request):
     return redirect('/screens')
 
 @login_required
-@staff_member_required
+##@staff_member_required
 def deleteScreen(request):
     # Delete a screen db entry
     screenid = request.POST.getlist("Delete")[0]
@@ -395,7 +395,7 @@ def get_mp4_thumbnail(file="./static/content/animals-Imgur.mp4"):
     return thumbnail
 
 @login_required
-@staff_member_required
+##@staff_member_required
 def make_video(request):
 
     videoFilePath = make_video_worker()
@@ -444,7 +444,7 @@ def video_compile_status(request):
 #_______________________________________________________________________________
 # Settings Managemnt Routines:
 @login_required
-@staff_member_required # Apply selection of images
+##@staff_member_required # Apply selection of images
 def apply_changes(request):
     changes = request.POST
     #log_applied_changes(changes)
@@ -526,7 +526,7 @@ def log_applied_changes(changes):
         print("Whoops")
 
 @login_required
-@staff_member_required
+##@staff_member_required
 def getContent(request):
     #print(File.objects.all())
     rem_addr = request.META.get('REMOTE_ADDR')
